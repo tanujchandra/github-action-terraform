b28e1a58517cf47398cfc872589452869839aa7d
tanujchandra <tanujchandra@gmail.com>
2026-07-15 21:36:17 +0530

    Add new Azure resource group 'rg_tanuj2'

--- rg.tf
+++ rg.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "rgs" {
   name     = "rg_tanuj"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "rgs" {
+  name     = "rg_tanuj2"
+  location = "West Europe"
+}
