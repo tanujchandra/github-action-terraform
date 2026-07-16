9b5d5dafc979086f151ec4a58f9ed0ac617ac989
Tanuj Chandra <tanujchandra@gmail.com>
2026-07-16 12:33:52 +0530

    rg name

--- rg.tf
+++ rg.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "rgs" {
   name     = "rg_tanuj007"
   location = "West Europe"
 }
-resource "azurerm_resource_group" "rgs" {
+resource "azurerm_resource_group" "rg007" {
   name     = "rg_tanuj2"
   location = "West Europe"
 }
